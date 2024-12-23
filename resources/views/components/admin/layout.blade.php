8411b2364d008af144d1cd0cd80ee0b12575fec1
coreoceantesting <coreoceantesting@gmail.com>
2024-12-23 11:43:56 +0530

    change title

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>{{ config('app.name') }} | {{ $title }}</title>
+    <title>UMC-SlotBooking | {{ $title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
